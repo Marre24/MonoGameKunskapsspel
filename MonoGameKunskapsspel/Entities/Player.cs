040ac8233fe09bea389efdfb2c71b2c121d1569b
maxi <maxi.ellnestam@gmail.com>
2022-12-11 19:14:35 +0100

    Start to reworking the game

---
+++ MonoGameKunskapsspel/Entities/Player.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Xna.Framework.Content;
+
+namespace MonoGameKunskapsspel
+{
+    class Player
+    {
+        SpriteBatch spriteBatch;
+        GraphicsDeviceManager graphicsDevice;
+
+        private Texture2D front;
+        private Texture2D back;
+        private Texture2D right;
+        private Texture2D left;
+        public Texture2D activeTexture;
+
+        Vector2 position;
+        Rectangle hitBox;
+        public Player(SpriteBatch spriteBatch, GraphicsDeviceManager graphicsDevice, ContentManager content)
+        {
+            this.spriteBatch = spriteBatch;
+            this.graphicsDevice = graphicsDevice;
+
+            front = content.Load<Texture2D>("RobotFront");
+            back = content.Load<Texture2D>("RobotBack");
+            right = content.Load<Texture2D>("RobotRight");
+            left = content.Load<Texture2D>("RobotLeft");
+
+            activeTexture = front;
+            position = new Vector2(100f,100f);
+            hitBox = new Rectangle();
+        }
+
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            spriteBatch.Draw(activeTexture, new Rectangle((int)position.X, (int)position.Y, 50 , 100), Color.White);
+        }
+    }
+}
--- MonoGameKunskapsspel/Game1.cs
+++ MonoGameKunskapsspel/Game1.cs
@@ -8,6 +8,8 @@ namespace MonoGameKunskapsspel
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Player player;
+
 
         public Game1()
         {
@@ -18,16 +20,14 @@ namespace MonoGameKunskapsspel
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            player = new Player(_spriteBatch, _graphics, Content);
 
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,7 +35,9 @@ namespace MonoGameKunskapsspel
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+
+            player.Update(gameTime);
+            
 
             base.Update(gameTime);
         }
@@ -44,9 +46,12 @@ namespace MonoGameKunskapsspel
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
 
-            base.Draw(gameTime);
+            player.Draw(gameTime);
+
+
+            _spriteBatch.End();
         }
     }
 }
